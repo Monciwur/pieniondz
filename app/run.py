1cacd1d7f3d75e843b1d1738eeb2c4fcea4190d8
Monciwur <krymek@gmail.com>
2017-08-08 20:34:01 +0200

    add tabs

--- app/run.py
+++ app/run.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/", methods=["GET", "POST"])
+def mainPage():
+    if request.method == "GET":
+        return render_template("mainPage.html")
+    if request.method == "POST":
+        product = request.form[u"produkt"]
+        zlotyAndGrosze = int((float(request.form[u"cenazl"]) * 100) + float(request.form[u"cenagr"]))
+        konChkBox = bool(request.form.getlist("konChkBox"))
+        matChkBox = bool(request.form.getlist("matChkBox"))
+        szyChkBox = bool(request.form.getlist("szyChkBox"))
+        print(konChkBox)
+        return redirect(url_for("mainPage"))
 
 if __name__ == '__main__':
     app.run()
